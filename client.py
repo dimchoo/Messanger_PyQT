be8c3048419f8ff96fc3f975a877ba8e8259c9af
dimchoo <imdimchoo@gmail.com>
2019-08-01 14:48:49 +0300

    Изменение структуры проекта

--- client.py
+++
@@ -1,6 +0,0 @@
-from base_classes import BaseClient
-
-client = BaseClient()
-
-if __name__ == '__main__':
-    client.start()
--- server.py
+++
@@ -1,9 +0,0 @@
-from base_classes import BaseServer
-from jim.utils import server_arg_parser
-
-listen_address, listen_port = server_arg_parser()
-
-server = BaseServer(listen_address, listen_port)
-
-if __name__ == '__main__':
-    server.start()
